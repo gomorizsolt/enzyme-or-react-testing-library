f2cfe0f153b5a12a54d13f0739e961e423d06b5d
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-09-29 18:37:35 +0200

    Remove comments

--- tests/find-elements/find-elements.rtl.test.js
+++ tests/find-elements/find-elements.rtl.test.js
@@ -2,21 +2,17 @@ import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import FindElementsFoo from "../../components/find-elements";
 
-// https://testing-library.com/docs/react-testing-library/api#cleanup
 afterEach(cleanup);
 
 describe("These test suite(s) is/are designed to demonstrate a few methods to access certain elements in the DOM", () => {
     it("getByLabelText", () => {
-        // https://testing-library.com/docs/dom-testing-library/api-queries#bylabeltext
         const { getByLabelText } = render(<FindElementsFoo />);
     
-        // https://testing-library.com/docs/dom-testing-library/api-queries#textmatch
         expect(getByLabelText("Username")).toBeInTheDocument();
         expect(getByLabelText(/user/i)).toBeInTheDocument();
     });
 
     it("getByTestId", () => {
-        // https://testing-library.com/docs/dom-testing-library/api-queries#bytestid
         const { getByTestId } = render(<FindElementsFoo />);
 
         expect(getByTestId("username-label")).toBeInTheDocument();
@@ -24,7 +20,6 @@ describe("These test suite(s) is/are designed to demonstrate a few methods to ac
     });
 
     it("getByPlaceholderText", () => {
-        // https://testing-library.com/docs/dom-testing-library/api-queries#byplaceholdertext
         const { getByPlaceholderText } = render(<FindElementsFoo />);
 
         expect(getByPlaceholderText("Username")).toBeInTheDocument();
