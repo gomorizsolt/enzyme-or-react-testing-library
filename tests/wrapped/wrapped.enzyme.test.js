675b3755a097f58f944e1d5e45942eb78532f410
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-10-01 21:50:09 +0200

    @skyboyer007 - use dive() instead of shallowWrappedComponent

--- tests/wrapped/wrapped.enzyme.test.js
+++ tests/wrapped/wrapped.enzyme.test.js
@@ -3,15 +3,10 @@ import { shallow } from "enzyme";
 import WrappedFoo from "../../components/wrapped";
 
 describe("These test suite(s) is/are designed to demonstrate how wrapped components can be tested", () => {
-    const shallowWrappedComponent = wrappedComponent =>
-    shallow(shallow(wrappedComponent).get(0));
-
-    // Shallow rendering would fail in this case, because merely the exterior surface of the HOC is rendered, nor the wrapped component.
-    // Full DOM rendering passes, but renders the whole tree.
-
     it("renders a <h1 />", () => {
-        // Shallow rendering is possible if the component is accessed one level deeper, however, it would definitely fail if more than one HOC is applied to the component.
-        const wrappedFooWrapper = shallowWrappedComponent(<WrappedFoo />);
+        // Shallow rendering would fail in this case, because merely the exterior surface of the HOC is rendered, nor the wrapped component.
+        // Full DOM rendering passes, but renders the whole tree.
+        const wrappedFooWrapper = shallow(<WrappedFoo />).dive();
 
         expect(wrappedFooWrapper.find("h1")).toHaveLength(1);
     });
